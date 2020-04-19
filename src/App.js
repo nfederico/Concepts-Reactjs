b8a384872983d9bd2b27f32128e080c01aa2371f
nfederico <nfederico@live.com>
2020-04-19 18:34:52 -0300

    All test passed with some enhacements

--- src/App.js
+++ src/App.js
@@ -25,7 +25,8 @@ function App() {
   }
 
   async function handleRemoveRepository(id) {
-    setRepositories([...repositories].splice(id,1));
+    await api.delete(`repositories/${id}`);
+    setRepositories(repositories.filter(repository => repository.id != id));
   }
 
   return (
@@ -34,7 +35,7 @@ function App() {
         {repositories.map(repository => ( 
           <li key={repository.id}>
                    {repository.title}
-                   <button onClick={() => handleRemoveRepository(1)}>Remover </button>
+                   <button onClick={() => handleRemoveRepository(repository.id)}>Remover </button>
           </li>
           ))}         
       
